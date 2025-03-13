9cb92e837d1099b6e1363ee26abeebf8469159f8
Th0masDI0N <thomas.dion2@etu.toulouse-inp.fr>
2025-03-13 11:24:46 +0100

    tpV2 pour généré les tables pour le rendu

--- TP1/krylov.m
+++ TP1/krylov.m
@@ -46,6 +46,7 @@ j = 1;
 % x initial (juste utile si on appelle la fonction sans avoir compléter le code : on a tous les résultats déclarés)
 x = x0;
 
+
 while (j <= maxit && relres > tol)
     
     % w = Av_j
@@ -54,7 +55,7 @@ while (j <= maxit && relres > tol)
     % orthogonalisation (Modified Gram-Schmidt)
     for i = 1:j
         H(i, j) = V(:,i)'*w;
-        w = w - H(i, j) * V(:, i)
+        w = w - H(i, j) * V(:, i);
     end 
     
     % calcul de H(j+1, j) et normalisation de V(:, j+1)
@@ -74,30 +75,22 @@ while (j <= maxit && relres > tol)
         % Estimation du résidu
         ej = zeros(j, 1);
         ej(j) = 1;
-        estimateur = norm( H(j+1, j) * ( ej'*y) * V(:, j+1) )
+        estimateur = norm( H(j+1, j) * ( ej'*y) * V(:, j+1) );
     else
-        % GMRES
-        % résolution du problème aux moindres carrés argmin ||beta.e1 - H_barre.y||
-        % construction de beta.e1 (taille j+1)
+        % GMRES  
         e1 = zeros(j+1, 1);
         e1(1) = beta;
-        % construction de H_barre
+
         H_barre = H(1:j+1, 1:j);
         % application des rotations de Givens à H_barre pour le rendre triangulaire supérieur
-        % for i = 1:j
-        %     % calcul de la rotation de Givens
-        %     c = H_barre(i, i) / sqrt(H_barre(i, i)^2 + H_barre(i+1, i)^2);
-        %     s = H_barre(i+1, i) / sqrt(H_barre(i, i)^2 + H_barre(i+1, i)^2);
-        %     % application de la rotation à H et à y
-        %     H_barre([i, i+1], i:j) = [c, s; -s, c] * H_barre([i, i+1], i:j);
-        %     e1([i, i+1]) = [c, s; -s, c] * e1([i, i+1]);
-        % end
-        % calcul de la rotation de Givens
-        c = H_barre(j, j) / sqrt(H_barre(j, j)^2 + H_barre(j+1, j)^2);
-        s = H_barre(j+1, j) / sqrt(H_barre(i, i)^2 + H_barre(i+1, i)^2);
-        % application de la rotation à H et à y
-        H_barre([i, i+1], i:j) = [c, s; -s, c] * H_barre([i, i+1], i:j);
-        e1([i, i+1]) = [c, s; -s, c] * e1([i, i+1]);
+        for i = 1:j
+            % calcul de la rotation de Givens
+            c = H_barre(i, i) / sqrt(H_barre(i, i)^2 + H_barre(i+1, i)^2);
+            s = H_barre(i+1, i) / sqrt(H_barre(i, i)^2 + H_barre(i+1, i)^2);
+            % application de la rotation à H et à y
+            H_barre([i, i+1], i:j) = [c, s; -s, c] * H_barre([i, i+1], i:j);
+            e1([i, i+1]) = [c, s; -s, c] * e1([i, i+1]);
+        end
         % resolution de H_barre.y = beta.e1 avec '\'
         y = H_barre(1:j+1, 1:j) \ (beta * e1);
         % Estimation du résidu
@@ -109,7 +102,7 @@ while (j <= maxit && relres > tol)
     % calcul de la norme du résidu et rangement dans resvec
     %resvec(j+1) = norm(b - A*x);
   
-    fprintf("size y: %d\n", size(y));
+    %fprintf("size y: %d\n", size(y));
     resvec(j+1) = estimateur;
     % calcul de la norme relative du résidu (backward error) relres
     relres = resvec(j+1) / normb;
---
+++ TP1/tpV2.m
@@ -0,0 +1,62 @@
+close all;
+clear all;
+
+
+%load mat1;
+%load pde225_5e-1;
+load hydcar20.mat;
+n = size(A, 1);
+fprintf('Dimension de A : %4d \n', n);
+
+
+b = [1:n]';   % Le vecteur b de taille n
+x0 = zeros(n, 1); % Point de départ initial
+kmax = n-1;     % Nombre maximum d'itérations
+A_dense = full(A);
+% Calculer le nombre de conditionnement de A_dense
+condition_number = cond(A_dense)
+% Initialisation d'un tableau pour stocker les résultats
+results = table(); % Table vide pour stocker les résultats
+iteration_count = 0; % Compteur d'itérations pour les résultats
+
+%% Test pour différentes tolérances
+tolerances = [1e-6, 1e-8, 1e-10];
+
+for tol_idx = 1:length(tolerances)
+    eps = tolerances(tol_idx); % Met à jour la tolérance
+
+    % FOM
+    [x_fom, flag_fom, relres_fom, iter_fom, resvec_fom] = krylov(A, b, x0, eps, kmax, 0);
+    iteration_count = iteration_count + 1;
+    results(iteration_count, :) = {eps, 'FOM', iter_fom, relres_fom, flag_fom};
+
+    % GMRES
+    [x_gmres, flag_gmres, relres_gmres, iter_gmres, resvec_gmres] = krylov(A, b, x0, eps, kmax, 1);
+    iteration_count = iteration_count + 1;
+    results(iteration_count, :) = {eps, 'GMRES', iter_gmres, relres_gmres, flag_gmres};
+
+    % GMRES MATLAB
+    [x_gmres_matlab, flag_gmres_matlab, relres_gmres_matlab, iter_gmres_matlab, resvec_gmres_matlab] = gmres(A, b, [], eps, kmax, [], [], x0);
+    iteration_count = iteration_count + 1;
+    results(iteration_count, :) = {eps, 'GMRES MATLAB', iter_gmres_matlab(2), relres_gmres_matlab, flag_gmres_matlab};
+
+    % Graphiques
+    figure;
+    semilogy(resvec_fom, 'c', 'DisplayName', 'FOM');
+    hold on;
+    semilogy(resvec_gmres, 'r', 'DisplayName', 'GMRES');
+    semilogy(resvec_gmres_matlab, 'b+', 'DisplayName', 'GMRES MATLAB');
+    legend('show');
+    title(['Évolution de la norme du résidu pour \epsilon = ', num2str(eps)]);
+    xlabel('Nombre d\ itérations');
+    ylabel('Norme du résidu');
+
+    % Enregistrement de la figure dans un fichier PNG
+    saveas(gcf, ['residu_epsilon_' num2str(eps) '.png']);
+end
+
+% Enregistrement des résultats dans un fichier CSV
+writetable(results, 'krylov_results.csv');
+
+% Résumé des résultats
+disp('Les résultats ont été enregistrés dans "krylov_results.csv".');
